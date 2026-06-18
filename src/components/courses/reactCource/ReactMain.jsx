c72c2f17a25f6b3f3f1472fce1a6d6a4c5d96669
Narendra Kadiveti <naninarendra2000@gmail.com>
2026-06-18 16:19:37 +0530

    Fix import paths

--- src/components/courses/reactCource/ReactMain.jsx
+++ src/components/courses/reactCource/ReactMain.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "../../common/Navbar";
 import Footer from "../../common/Footer";
 
-import ReactHeroSection from "./ReactHeroSection";
+import ReactHeroSection from "./ReactHeroSection.jsx";
 import ReactCourse from "./ReactCourse";
 import ReactMentors from "./ReactMentors";
 import ReactProjectsSection from "./ReactProjectsSection";
